869a63b5ab8dfaf9eabc7d99228c1a473124b2e1
Dudon Wai <dudonwai@gmail.com>
2015-09-07 15:00:08 -0600

    Create initial settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dudonsblog.wsgi --log-file -
--- dudonsblog/settings.py
+++ dudonsblog/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+# Prase database configurationfrom $DATABASE_URL
+import dj_database_url
+DATABASES[default'] = dj_database_url.config()
+
+# Honour the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+#Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/

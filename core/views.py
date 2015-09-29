b2eec43703f43389ce63bb53d2d05e215e31d7db
Dudon Wai <dudonwai@gmail.com>
2015-09-28 21:30:23 -0600

    Adding BlogListView to Landing Page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+import core.models as coremodels
+from django.views.generic.edit import CreateView, UpdateView
+from sitegate.decorators import redirect_signedin, sitegate_view
 
 # Create your views here.
 
 class LandingView(TemplateView):
 	template_name = "base/index.html"
 
+class BlogListView(ListView):
+    model = coremodels.Location
+    template_name = 'location/list.html'
 	
+
+class LocationListView(ListView):
+    model = coremodels.Location
+    template_name = 'location/list.html'
+    paginate_by = 5

f88762107a3653e9c8940e91a789ba78506c058e
Dudon Wai <dudonwai@gmail.com>
2015-09-29 21:54:08 -0600

    Testing out how to make blog pages. Stable landing page

--- core/views.py
+++ core/views.py
@@ -4,19 +4,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 import core.models as coremodels
 from django.views.generic.edit import CreateView, UpdateView
-from sitegate.decorators import redirect_signedin, sitegate_view
+
 
 # Create your views here.
 
 class LandingView(TemplateView):
 	template_name = "base/index.html"
 
-class BlogListView(ListView):
-    model = coremodels.Location
-    template_name = 'location/list.html'
-	
 
-class LocationListView(ListView):
-    model = coremodels.Location
-    template_name = 'location/list.html'
-    paginate_by = 5

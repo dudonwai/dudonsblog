370af7fc198c9866a8bc924af7848fa98ae7e270
Dudon Wai <dudonwai@gmail.com>
2015-10-09 23:20:54 -0600

    Added static files

--- dudonsblog/settings.py
+++ dudonsblog/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'core',
     'bootstrap_pagination',
 )

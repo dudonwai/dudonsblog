7343835cb582af19f1a34fae577af786b3d18917
Dudon Wai <dudonwai@gmail.com>
2015-10-12 22:21:35 -0600

    Adding First Blog

--- dudonsblog/settings.py
+++ dudonsblog/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',
 )
 
 MIDDLEWARE_CLASSES = (

75063af20949a72faafed25f99ad8c2673bc822c
Dudon Wai <dudonwai@gmail.com>
2015-11-01 21:50:50 -0700

    Linking first blog post

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ import core.views as coreviews
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'index', name='index', coreviews.BlogView.as_view()),
+    url(r'^$', coreviews.BlogView.as_view()),
     url(r'^post/list.html', coreviews.PostListView.as_view()),
     url(r'^blog/20150930', coreviews.Blog20150930View.as_view()),
 )

cea812fde65561f1c57187d40fa3ad62e6ad3285
Dudon Wai <dudonwai@gmail.com>
2015-11-01 19:46:05 -0700

    Linking first blog post

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
 
     url(r'^$', coreviews.BlogView.as_view()),
     url(r'^post/list.html', coreviews.PostListView.as_view()),
-    url(r'^blog/20150930.html', coreviews.Blog20150930View.as_view()),
+    url(r'^blog/20150930', coreviews.Blog20150930View.as_view()),
 
 )

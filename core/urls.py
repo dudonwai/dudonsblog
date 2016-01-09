3b646e303525d6facb28840230d3be4e1ba36248
dudonwai <dudonwai@gmail.com>
2016-01-09 16:19:20 -0700

    Adding links, and separate pages for about and blog

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ import core.views as coreviews
 urlpatterns = patterns('',
 
     url(r'^$', coreviews.LandingView.as_view()),
-    url(r'^blog/20150930-whyblog', coreviews.Blog20150930View.as_view()),
-	url(r'^blog/20151025-education', coreviews.BlogView.as_view()),
+    url(r'^blog/about', coreviews.BlogView.as_view()),
+    url(r'^blog/blog', coreviews.BlogView.as_view()),
 )

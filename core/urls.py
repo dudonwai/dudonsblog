39512d76ce3724d878914c333f3bd8a65a81f872
dudonwai <dudonwai@gmail.com>
2016-01-25 21:16:37 -0700

    Disable About.html

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ import core.views as coreviews
 urlpatterns = patterns('',
 
     url(r'^$', coreviews.LandingView.as_view()),
-    url(r'^about', coreviews.AboutView.as_view()),
+    # url(r'^about', coreviews.AboutView.as_view()),
     url(r'^blog', coreviews.BlogView.as_view()),
     # url(r'^startproject', coreviews.StartProjectView.as_view()),
 )

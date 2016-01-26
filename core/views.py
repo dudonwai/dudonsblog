8384edd7cc2065300de1b0fd556b3341dafe8edb
dudonwai <dudonwai@gmail.com>
2016-01-25 21:15:38 -0700

    Disable About.html

--- core/views.py
+++ core/views.py
@@ -21,9 +21,9 @@ class BlogView(ListView):
 	model = coremodels.Post
 	template_name = "blog/blog.html"
 
-class AboutView(ListView):
-	model = coremodels.Post
-	template_name = "blog/about.html"
+# class AboutView(ListView):
+# 	model = coremodels.Post
+# 	template_name = "blog/about.html"
 
 # class StartProjectView(ListView):
 # 	model = coremodels.Post
